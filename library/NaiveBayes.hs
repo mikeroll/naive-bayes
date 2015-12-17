efc8cedfc780c5be6cc6bd44a4bb0cf2b784c636
Mikhail Bulash <scaryspiderpig@gmail.com>
2015-12-17 14:33:00 +0300

    Classifier is here

--- library/NaiveBayes.hs
+++ library/NaiveBayes.hs
@@ -1,4 +1,70 @@
 module NaiveBayes
-(
+( splitDataset
 ) where
 
+import System.Random
+import Data.List
+import Data.List.Extras
+import Data.Map (toList, fromListWith)
+
+import Statistics
+
+type Feature = Double
+type Object  = [Feature]
+type Label   = String
+type Dataset = [(Object, Label)]
+
+data Class = Class
+    { label :: String
+    , aprioriP :: Double
+    , cMean :: Object
+    , cDispersion :: [Double]
+    }
+
+data Classifier = Classifier
+    { classes :: [Class]
+    }
+
+-- | Groups objects into classes
+classMap :: [(Object, Label)] -> [(Label, [Object])]
+classMap h = toList $ fromListWith (++) [(label, [obj]) | (obj, label) <- h]
+
+-- | Extracts available classes, centers and a priori probabilities from the training set
+makeClasses :: (Dataset, Dataset) -> [Class]
+makeClasses (train, test) = map makeClass $ classMap train
+    where
+        total = genericLength train + genericLength test
+        makeClass (label, objs) =
+            Class { label = label
+                  , aprioriP = genericLength objs / total
+                  , cMean = cmean
+                  , cDispersion = cdisp
+                  }
+          where
+            cmean = mean2d objs
+            cdisp = dispersion2d cmean objs
+
+--
+likelihood :: (Floating f) => Object -> Class -> Double
+likelihood x c = ap * pxc
+    where
+        ap  = aprioriP c
+        cms = cMean c
+        cds = cDispersion c
+        pxc = product $ zipWith3 normalDensity cms cds x
+
+-- | Tells the most appropriate class for an object
+tell :: Classifier -> Object -> Class
+tell model obj = argmax (likelihood obj) $ classes model
+
+-- | Splits a given dataset into train and test sets
+splitDataset :: (RandomGen g) => g -> Double -> Dataset -> (Dataset, Dataset)
+splitDataset g share set = branch share $ zip rs set
+    where
+        rs = randoms g :: [Double]
+        branch share [] = ([], [])
+        branch share ((v, x) : vxs)
+            | v < share = (x : train, test)
+            | otherwise = (train, x : test)
+          where
+            (train, test) = branch share vxs
--- library/Statistics.hs
+++ library/Statistics.hs
@@ -1,20 +1,31 @@
 module Statistics
 ( mean
+, mean2d
 , dispersion
+, dispersion2d
+, normalDensity
 ) where
 
 import Data.List
 
--- | Statistical mean of a list of objects
+-- | Statistical mean of a list of values
 mean :: Floating f => [f] -> f
 mean xs = sum xs / genericLength xs
 
--- | Statistical dispersion of a list of objects
+-- | Statistical dispersion of a list of values
 dispersion :: Floating f => f -> [f] -> f
-dispersion mean objects = 1 / (n - 1) * s
+dispersion mean xs = s / (n - 1)
     where
-        n = genericLength objects
-        s = sum $ map ((mean -) . (** 2)) objects
+        n = genericLength xs
+        s = sum $ map ((** 2) . (mean -)) xs
+
+-- | 2D version of `mean`
+mean2d :: Floating f => [[f]] -> [f]
+mean2d = map mean . transpose
+
+-- | 2D version of `dispersion`
+dispersion2d :: Floating f => [f] -> [[f]] -> [f]
+dispersion2d mean2d xs = zipWith dispersion mean2d (transpose xs)
 
 -- | Prabability density of the normal distribution
 normalDensity :: (Floating f) => f -> f -> f -> f
@@ -22,3 +33,4 @@ normalDensity mean disp x = exp power / denom
     where
         power = - (x - mean) ^ 2 / (2 * disp)
         denom = sqrt (2 * pi * disp)
+
